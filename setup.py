4d3e4bce29cadaef09ddd1c4f1c4c8d5696f83e9
Jacob Kaplan-Moss <jacob@jacobian.org>
2009-12-06 19:21:00 -0600

    Push 1.0a1 to pypi.

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-from distribute_setup import use_setuptools; use_setuptools()
-from setuptools import setup
+from distribute_setup import use_setuptools; use_setuptools()    
+from setuptools import setup, find_packages
 
 setup(
-    name = "cloudservers",
-    version = "1.0",
-    description = "Client library for Rackspace's Cloud Servers",
+    name = "python-cloudservers",
+    version = "1.0a1",
+    description = "Client library for Rackspace's Cloud Servers API",
+    url = 'http://packages.python.org/python-cloudservers',
     license = 'BSD',
     author = 'Jacob Kaplan-Moss',
-    author_email = 'jacob@jaobian.org',
-    py_modules = ['cloudservers'],
+    author_email = 'jacob@jacobian.org',
+    packages = find_packages(),
     classifiers = [
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
         'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
